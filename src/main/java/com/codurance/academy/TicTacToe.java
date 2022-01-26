927d2c9788979d8c82ce87a8fc65e723aeb80ac6
Lisandro Bertoli <lisandro.bertoli@gmail.com>
2022-01-26 16:44:44 +0100

    refactor caliwtf

--- src/main/java/com/codurance/academy/TicTacToe.java
+++ src/main/java/com/codurance/academy/TicTacToe.java
@@ -1,28 +1,42 @@
 package com.codurance.academy;
 
 public class TicTacToe {
+
+    public static final String EMPTY_LINE = "_|_|_";
+
     public String play(Point point) {
-        StringBuilder line = buildLine(point);
-
-        if(point.getX() == 1){
-            return "_|_|_\n" + line + "\n_|_|_";
-        }
-
-        if (point.getY() == 2) {
-            return "_|_|_\n" + "_|_|_\n" + line;
-        }
-        return line + "\n_|_|_\n" + "_|_|_";
+        return buildMatrix(point);
     }
 
-    private StringBuilder buildLine(Point point) {
+    private String buildMatrix(Point point) {
+        StringBuilder matrix = new StringBuilder();
+
+        for (int yIndex = 0; yIndex < 3; yIndex++) {
+            matrix.append(getLine(point, yIndex));
+
+            matrix.append(getEndOfLine(yIndex));
+        }
+
+        return matrix.toString();
+    }
+
+    private String getEndOfLine(int yIndex) {
+        return yIndex < 2 ? "\n" : "";
+    }
+
+    private String getLine(Point point, int yIndex) {
+        return yIndex == point.getY() ? buildLine(point) : EMPTY_LINE;
+    }
+
+    private String buildLine(Point point) {
         StringBuilder line = new StringBuilder();
 
-        for(int xIndex = 0; xIndex < 3; xIndex++){
+        for (int xIndex = 0; xIndex < 3; xIndex++) {
             line.append(getCellMarker(point, xIndex));
             line.append(getSeparator(xIndex));
         }
 
-        return line;
+        return line.toString();
     }
 
     private String getSeparator(int xIndex) {
