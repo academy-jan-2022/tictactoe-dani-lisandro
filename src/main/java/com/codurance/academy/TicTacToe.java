8a97b1bad99a1b58f9bc4e87569e713cd0ac737e
Lisandro Bertoli <lisandro.bertoli@gmail.com>
2022-01-26 16:19:23 +0100

    refactor

--- src/main/java/com/codurance/academy/TicTacToe.java
+++ src/main/java/com/codurance/academy/TicTacToe.java
@@ -2,8 +2,21 @@ package com.codurance.academy;
 
 public class TicTacToe {
     public String play(Point point) {
+        StringBuilder line = new StringBuilder();
+        for(int xIndex = 0; xIndex < 3; xIndex++){
+            if(xIndex == point.getX()){
+                line.append("X");
+            }else{
+                line.append("_");
+            }
+
+            if(xIndex < 2){
+                line.append("|");
+            }
+        }
+
         if(point.getX() == 1){
-            return "_|_|_" + "_|X|_" + "_|_|_";
+            return "_|_|_" + line + "_|_|_";
         }
 
         if (point.getY() == 2) {
