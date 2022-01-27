ef5c501c1947fc12d6dfb5a10e067330682ea977
danihra-codurance <daniel.rodriguez@codurance.com>
2022-01-27 10:39:53 +0100

    refactor 1

--- src/main/java/com/codurance/academy/TicTacToe.java
+++ src/main/java/com/codurance/academy/TicTacToe.java
@@ -8,34 +8,40 @@ public class TicTacToe {
     public static final String SEPARATOR = "\n";
     private Point previousPlay = null;
 
-    public String play(Point point) {
-        String playLine = buildLine(point, previousPlay);
-        previousPlay = point;
+    public String play(Point currentPlay) {
+        String playLine = buildLine(currentPlay, previousPlay);
+        previousPlay = currentPlay;
 
         return playLine + SEPARATOR + EMPTY_LINE + SEPARATOR + EMPTY_LINE;
     }
 
-    private String buildLine(Point point, Point previousPlay) {
+    private String buildLine(Point currentPlay, Point previousPlay) {
         StringBuilder line = new StringBuilder();
 
         for (int xIndex = 0; xIndex < 3; xIndex++) {
-            if (Objects.equals(previousPlay, new Point(xIndex, 0))) {
-                line.append("X");
-            } else if (Objects.equals(point, new Point(xIndex, 0))) {
-                line.append(previousPlay == null ? "X" : "O");
-            } else {
-                line.append("_");
-            }
-
+            line.append(getCurrentChar(currentPlay, previousPlay, xIndex));
             line.append(getSeparator(xIndex));
         }
 
         return line.toString();
+    }
 
+    private String getCurrentChar(Point currentPlay, Point previousPlay, int xIndex) {
+        if (isSamePoint(previousPlay, xIndex)) {
+            return "X";
+        }
+        if (isSamePoint(currentPlay, xIndex)) {
+            return previousPlay == null ? "X" : "O";
+        }
+
+        return "_";
+    }
+
+    private boolean isSamePoint(Point previousPlay, int xIndex) {
+        return Objects.equals(previousPlay, new Point(xIndex, 0));
     }
 
     private String getSeparator(int xIndex) {
         return xIndex < 2 ? "|" : "";
     }
-
 }
