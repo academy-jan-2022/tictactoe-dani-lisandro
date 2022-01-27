48125166887386370eca2ee2f2c178b1d7f081a5
Lisandro Bertoli <lisandro.bertoli@gmail.com>
2022-01-27 12:45:54 +0100

    refactor

--- src/main/java/com/codurance/academy/TicTacToe.java
+++ src/main/java/com/codurance/academy/TicTacToe.java
@@ -13,45 +13,29 @@ public class TicTacToe {
     public String play(Point currentPlay) {
         plays.add(currentPlay);
 
-        String playLine = buildLine(currentPlay);
+        String playLine = buildLine();
 
 
         return playLine + SEPARATOR + EMPTY_LINE + SEPARATOR + EMPTY_LINE;
     }
 
-    private String buildLine(Point currentPlay) {
+    private String buildLine() {
         StringBuilder line = new StringBuilder();
 
         for (int xIndex = 0; xIndex < 3; xIndex++) {
-            line.append(getCurrentChar(currentPlay, new Point(xIndex, 0)));
+            line.append(getCurrentChar(new Point(xIndex, 0)));
             line.append(getSeparator(xIndex));
         }
 
         return line.toString();
     }
 
-    private String getCurrentChar(Point currentPlay, Point xIndex) {
+    private String getCurrentChar (Point xIndex) {
         for (int i=0; i< plays.size(); i++){
             if(isSamePoint(plays.get(i), xIndex)){
                 return i % 2 == 0 ? "X" : "O";
             }
-
         }
-
-       /* if (isSamePoint(previousPreviousPlay, xIndex)) {
-            return "X";
-        }
-
-        if (isSamePoint(previousPlay, xIndex)) {
-            return previousPreviousPlay == null ? "X" : "O";
-        }
-
-        if (isSamePoint(currentPlay, xIndex)) {
-            if(previousPreviousPlay != null){
-                return "X";
-            }
-            return previousPlay == null ? "X" : "O";
-        }*/
 
         return "_";
     }
